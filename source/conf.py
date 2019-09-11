ef93fe1398cbcbafaef5bcb3a81364ee4e1b5b31
Andrew Huang <33182390+epadam@users.noreply.github.com>
2019-09-11 13:29:19 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-autodoc_mock_imports = ["sphinxcontrib.bibtex"]
+autodoc_mock_imports = ["sphinxcontrib-bibtex"]
 
 # -- Project information -----------------------------------------------------
 
